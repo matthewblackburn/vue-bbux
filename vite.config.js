a59c72037c4230e40d37aad89af3c7b9b5c2a7bc
matthewblackburn <blackburn.stack@gmail.com>
2022-07-05 09:56:07 +1000

    issues

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { fileURLToPath, URL } from "url";
-
 import { defineConfig } from "vite";
+
 import vue from "@vitejs/plugin-vue";
+import libCss from "vite-plugin-libcss";
 
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-        cssCodeSplit: true,
         lib: {
-            format: ["umd"],
             entry: path.resolve(__dirname, "src/index.js"),
             name: "vue-bbux",
             fileName: (format) => `vue-bbux.${format}.js`,
@@ -25,10 +24,10 @@ export default defineConfig({
             },
         },
     },
-    plugins: [vue()],
-    // resolve: {
-    //     alias: {
-    //         "@": fileURLToPath(new URL("./src", import.meta.url)),
-    //     },
-    // },
+    plugins: [vue(), libCss()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
 });
